933f99b6af2b597eeebefcceee5b8fa38756fbf5
lgm@nemesis.co.il <lgm@nemesis.co.il>
2020-04-18 15:39:49 +0300

    angular fire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,11 +10,18 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { AngularFireModule } from '@angular/fire';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+  ],
   providers: [
     StatusBar,
     SplashScreen,
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBIc6xJd9MopPMgCATSHz4VLAUIsIyJae0",
+    authDomain: "reconnect-i.firebaseapp.com",
+    databaseURL: "https://reconnect-i.firebaseio.com",
+    projectId: "reconnect-i",
+    storageBucket: "reconnect-i.appspot.com",
+    messagingSenderId: "15338544369",
+    appId: "1:15338544369:web:870238a038bbc520561d50",
+    measurementId: "G-XPLGQ5LX96"
+  }
 };
 
 /*
