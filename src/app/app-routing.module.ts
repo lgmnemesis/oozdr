d8a27c7f2d2949014b433597305bdec11585e12e
lgm@nemesis.co.il <lgm@nemesis.co.il>
2020-04-20 09:19:36 +0300

    auth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './core/auth.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: 'start', pathMatch: 'full' },
   {
     path: 'start',
-    loadChildren: () => import('./pages/start/start.module').then( m => m.StartPageModule)
+    loadChildren: () => import('./pages/start/start.module').then( m => m.StartPageModule),
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
   },
   // Default redirect route rule if non of the above matches
-  { path: '**', redirectTo: 'start', pathMatch: 'full' }
+  { path: '**', redirectTo: 'start', pathMatch: 'full' },
 ];
 
 @NgModule({
