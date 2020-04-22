14bfcc7ecdc1e1453e47bc8f8ba2c026eae9479d
lgm@nemesis.co.il <lgm@nemesis.co.il>
2020-04-22 13:56:26 +0300

    good

--- src/app/components/welcome/welcome-container/welcome-container.component.ts
+++ src/app/components/welcome/welcome-container/welcome-container.component.ts
@@ -9,15 +9,34 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 export class WelcomeContainerComponent implements OnInit {
 
   name = '';
+  gender = '';
   email = '';
-  selectedGender = '';
+
+  nameError = 'no errors';
+  isNameError = false;
+  genderError = 'no errors';
+  isGenderError = false;
+  emailError = 'no errors';
+  isEmailError = false;
 
   constructor() { }
 
   ngOnInit() {}
 
+  setName(event) {
+    this.name = event.detail.value;
+    this.isValidName();
+  }
+
   setGender(gender: string) {
-    this.selectedGender = gender;
+    this.gender = gender;
+    this.genderError = 'no errors';
+    this.isGenderError = false;
+  }
+
+  setEmail(event) {
+    this.email = event.detail.value;
+    this.isValidEmail();
   }
 
   nextStep() {
@@ -26,8 +45,47 @@ export class WelcomeContainerComponent implements OnInit {
     }
   }
 
+  isValidName() {
+    this.nameError = 'no errors';
+    this.isNameError = false;
+    const name = this.name.trim();
+    if (!name) {
+      this.nameError = 'Please enter your name';
+      this.isNameError = true;      
+    } else if (name.length < 2) {
+      this.nameError = 'Sorry, name is too short';
+      this.isNameError = true;
+    }
+  }
+
+  isValidGender() {
+    this.genderError = 'no errors';
+    this.isGenderError = false;
+    if (this.gender === '') {
+      this.genderError = 'Please selecet a gender';
+      this.isGenderError = true;
+    }
+  }
+
+  isValidEmail() {
+    this.emailError = 'no errors';
+    this.isEmailError = false;
+    const email = this.email.trim();
+    if (!email) {
+      this.emailError = 'Please enter your email';
+      this.isEmailError = true;      
+    } else if (!email.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)) {
+      this.emailError = 'Invalid email address';
+      this.isEmailError = true;
+    }
+  }
+
   isValidatedForm() {
-    const isValidated = false;
+    this.isValidName();
+    this.isValidGender();
+    this.isValidEmail();
+    const isValidated = !this.isNameError && !this.isGenderError && !this.isEmailError;
+    console.log('isValidated:', isValidated);
     return isValidated;
   }
 }
