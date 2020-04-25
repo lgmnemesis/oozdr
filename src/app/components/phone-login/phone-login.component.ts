f82ef404ceaf6afaa138643ef0bf414c87ace1f5
lgm@nemesis.co.il <lgm@nemesis.co.il>
2020-04-25 14:22:55 +0300

    yalla

--- src/app/components/phone-login/phone-login.component.ts
+++ src/app/components/phone-login/phone-login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ChangeDetectionStrategy, ChangeDetectorRef, OnDestroy, Input } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, ChangeDetectorRef, OnDestroy } from '@angular/core';
 import * as firebase from 'firebase/app';
 import 'firebase/auth';
 import { AngularFireAuth } from '@angular/fire/auth';
