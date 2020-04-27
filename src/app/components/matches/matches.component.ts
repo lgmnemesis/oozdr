45c9307f6ea5cc89bad94fa498744b20125ace02
lgm@nemesis.co.il <lgm@nemesis.co.il>
2020-04-27 13:03:49 +0300

    connection

--- src/app/components/matches/matches.component.ts
+++ src/app/components/matches/matches.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit, OnDestroy, ChangeDetectionStrategy } from '@angular/core';
 import { SharedService } from 'src/app/services/shared.service';
 
+export class Q {
+  name = '';
+  phoneNumber = '';
+  letThemKnow = false;  
+}
+
 @Component({
   selector: 'app-matches',
   templateUrl: './matches.component.html',
@@ -9,11 +15,10 @@ import { SharedService } from 'src/app/services/shared.service';
 })
 export class MatchesComponent implements OnInit, OnDestroy {
 
-  name = '';
+  Q = new Q();
   isNameError = false;
   nameError = 'no errors';
   telInputObj: any
-  phoneNumber = '';
   phoneError = 'no errors';
   isPhoneError = false;
   countryCode = this.sharedService.defaultPhoneCountryCode || this.sharedService.INITIAL_PHONE_COUNTRY_CODE;
@@ -23,13 +28,13 @@ export class MatchesComponent implements OnInit, OnDestroy {
   ngOnInit() {}
 
   setName(event) {
-    this.name = event.detail.value;
+    this.Q.name = event.detail.value;
   }
 
   getAndVerifyNumber(): boolean {
     try {
       const isValid = this.telInputObj.isValidNumber();
-      this.phoneNumber = this.telInputObj.getNumber();
+      this.Q.phoneNumber = this.telInputObj.getNumber();
       return isValid;
     } catch (error) {
       console.error(error);
@@ -44,6 +49,15 @@ export class MatchesComponent implements OnInit, OnDestroy {
   telHasError(event) {
     console.log('telHasError:', event);
     console.log('detailed error:', this.telInputObj.getValidationError());
+  }
+
+  toggleLetThemKnow(event) {
+    console.log(event);
+    this.Q.letThemKnow = event.detail.value;
+  }
+
+  addConnection() {
+
   }
 
   ngOnDestroy() {
