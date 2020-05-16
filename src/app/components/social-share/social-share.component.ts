0a91725b67fe7462325fc294a314c032ebe02b37
lgm@nemesis.co.il <lgm@nemesis.co.il>
2020-05-16 14:47:50 +0300

    adding navigation.share support + deploy

--- src/app/components/social-share/social-share.component.ts
+++ src/app/components/social-share/social-share.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SocialShareComponent implements OnInit {
 
+  navigatorShareSupport = false;
+
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    const nav: any = navigator;
+    this.navigatorShareSupport = nav.share;
+  }
 
   shareOnWhatsapp() {
 
@@ -30,4 +35,20 @@ export class SocialShareComponent implements OnInit {
   copyLink() {
 
   }
+
+  async navigatorShare() {
+    const nav: any = navigator;
+    const share = {
+      url: 'https://reconnect.page.link/invite',
+      title: 'App recommendation for you.',
+      text: 'This app has helped me to reconnect with some one dear and i think it can help you as well.'
+    }
+    try {
+      await nav.share(share);
+      console.log('Thanks for sharing!')
+    } catch (error) {
+      console.error('Could not share!', error);
+    }
+  }
+
 }
