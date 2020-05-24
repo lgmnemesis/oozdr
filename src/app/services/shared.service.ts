9a729616a146406fafb4f20a2d779a6d1ee3edbd
lgm@nemesis.co.il <lgm@nemesis.co.il>
2020-05-24 18:01:51 +0300

    fixes for oozdr

--- src/app/services/shared.service.ts
+++ src/app/services/shared.service.ts
@@ -19,7 +19,7 @@ export class SharedService {
 
   INITIAL_PHONE_COUNTRY_CODE = 'us';
 
-  dynamicLinkInvitationUrl = 'https://reconnect.page.link/invitation';
+  dynamicLinkInvitationUrl = 'https://oozdr.page.link/invitation';
   ipInfoUrl = 'https://ipapi.co/json';
   defaultPhoneCountryCode: string = null;
   countryCodeStoreKeyName = 'country_code';
