e52951f01b5eaea796b97963b60008c467ed018d
lgm@nemesis.co.il <lgm@nemesis.co.il>
2020-06-12 22:01:06 +0300

    good

--- src/app/components/manage-connection-modal/manage-connection-modal.component.ts
+++ src/app/components/manage-connection-modal/manage-connection-modal.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
 import { ConnectionsState } from 'src/app/interfaces/connections-state';
-import { ModalController } from '@ionic/angular';
+import { ModalController, AlertController } from '@ionic/angular';
 import { ContactPickerApiService } from 'src/app/services/contact-picker-api.service';
 
 @Component({
@@ -14,10 +14,12 @@ export class ManageConnectionModalComponent implements OnInit {
   connectionsState: ConnectionsState;
   contacts = null;
   title =  '';
+  showHelpLock = false;
 
   constructor(private modalCtrl: ModalController,
     public contactPickerApiService: ContactPickerApiService,
-    private cd: ChangeDetectorRef) { }
+    private cd: ChangeDetectorRef,
+    private alertCtrl: AlertController) { }
 
   ngOnInit() {
     const state = this.connectionsState && this.connectionsState.state ? this.connectionsState.state : null;
@@ -36,6 +38,41 @@ export class ManageConnectionModalComponent implements OnInit {
   gotContacts(event) {
     this.contacts = event;
     this.markForCheck();
+  }
+
+  async showHelp() {
+    if (this.showHelpLock) return;
+    this.showHelpLock = true;
+
+    const alert = await this.alertCtrl.create({
+      header: `Adding Closure`,
+      message:
+`
+Writing a closure message can benefit you as well as your EX...
+
+Just like Beats, your closure will be matched and shown only if the other person is looking for you too (whether it is to reconnect or to give you their own closure message)
+
+The goal is to share your feelings about your relationship, 
+your breakup, and your thoughts about the future. 
+It meant to allow you to accept what happened, say goodbye, and move on.
+
+`,
+      mode: 'ios',
+      cssClass: 'manage-connection-help',
+      buttons: [
+        {
+          text: 'Got It',
+          handler: () => {
+            this.showHelpLock = false;
+          }
+        }
+      ]
+    });
+
+    alert.onDidDismiss().finally(() => {
+      this.showHelpLock = false;
+    });
+    await alert.present();
   }
 
   close() {
