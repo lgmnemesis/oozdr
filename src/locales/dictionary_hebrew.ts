11413a7b6f263e3b2ccb69fb7235193478ef7538
lgm@nemesis.co.il <lgm@nemesis.co.il>
2020-06-15 20:44:26 +0300

    more

--- src/locales/dictionary_hebrew.ts
+++ src/locales/dictionary_hebrew.ts
@@ -1,27 +1,27 @@
 export const DictionaryHebrew = {
   global: {
-    log_in: 'Log In',
-    sign_up: 'Sign up',
-    already_member: 'Already a member?'
+    log_in: 'כניסה',
+    sign_up: 'הרשמה',
+    already_member: 'משתמש קיים?'
   },
   signInComponent: {
-    log_into_account: 'Log into your account',
-    no_account: 'Don\'t have an account? ',
+    log_into_account: 'כניסה לחשבון',
+    no_account: 'משתמש אינו קיים? ',
   },
   appComponent: {
-    menu_1_1: 'Profile',
-    menu_1_2: 'Settings',
-    menu_1_3: 'Install App',
-    menu_1_4: 'Invite Your Friends',
-    menu_1_5: 'Log Out',
-    menu_2_1: 'Beats',
-    menu_2_2: 'Matches'
+    menu_1_1: 'פרופיל',
+    menu_1_2: 'הגדרות',
+    menu_1_3: 'התקנת אפליקציה',
+    menu_1_4: 'שתפו מכרים',
+    menu_1_5: 'יציאה',
+    menu_2_1: 'ביטים',
+    menu_2_2: 'התאמות'
   },
   supportTxt: {
-    how_title: 'How It Works',
-    section1_title: 'Add Beat(s)',
-    section1_1: 'Fill in the name and mobile number of the person you \'secretly\' want to reconnect with.',
-    section1_2: 'We will NOT let them know you are looking for them at this point.',
+    how_title: 'איך זה עובד',
+    section1_title: 'הוספת ביט',
+    section1_1: 'ממלאים שם ומספר נייד של האדם איתו רוצים לחדש קשר',
+    section1_2: 'האדם, בשלב זה, לא יהיה מודע לכך',
     section2_title: 'Wait for It',
     section2_1: 'This is the hard part. waiting... If at any time, the person you are looking for is looking for you too, we will match you up.',
     section3_title: 'You Have a Match',
@@ -45,78 +45,78 @@ export const DictionaryHebrew = {
     title_2_delete: 'the story of love is hello and goodbye... until we meet again”',
     title_3_delete: '― Jimi Hendrix',
     circles: {
-      c1: 'Closure',
-      c2: 'Soulmate',
-      c3: 'Reach Out',
-      c4: 'Feel',
-      c5: 'Touch'
+      c1: 'לגעת',
+      c2: 'נפש',
+      c3: 'להושיט יד',
+      c4: 'להרגיש',
+      c5: 'סגירת מעגל'
     }
   },
   siteFooterComponent: {
-    col_1_title: 'Legal',
+    col_1_title: ' ',
     col_1_1: 'Privacy Policy',
     col_1_2: 'Terms of Service',
-    col_2_title: 'Information',
-    col_2_1: 'Contact',
-    col_2_2: 'Support'
+    col_2_title: ' ',
+    col_2_1: 'יצירת קשר',
+    col_2_2: 'תמיכה'
   },
   phoneLoginComponent: {
     verifyAccount: 'Please Sign in again to verify your account',
-    title: 'Enter Your Mobile Number',
+    title: 'מספר הנייד שלך',
     explanation_1: 'We will send a text message with your verification code. Message and data rate may apply. The verified phone number will be used to login.',
     term_s1: 'By continuing, you agree to our ',
     term_s2: 'Terms',
-    continue_btn: 'Continue',
-    verifyCode: 'Enter Your Verification Code',
-    verifyCodePlaceholder: 'Verification Code',
+    continue_btn: 'המשך/י',
+    verifyCode: 'הכנס/י קוד אימות',
+    verifyCodePlaceholder: 'קוד אימות זמני',
     verify_btn: 'Verify',
-    phoneError_1: 'Please Enter Your Mobile Number',
-    phoneError_2: 'Invalid Number',
-    verificationError_1: 'Please Enter Your Verification Code',
-    verificationError_2: 'Wrong Verification Code'
+    phoneError_1: 'בבקשה מלא/י מספר נייד',
+    phoneError_2: 'מספר שגוי',
+    verificationError_1: 'בבקשה הכנס/י קוד אימות',
+    verificationError_2: 'הקוד שהוכנס שגוי'
   },
   welcomeContainerComponent: {
-    title: 'GET STARTED'
+    title: 'תהליך הרשמה'
   },
   welcomeInfoComponent: {
-    title: 'Personal Details',
-    input_1: 'Name',
-    input_1_1: 'Your Name',
-    input_2: 'Gender',
-    input_2_1: 'Male',
-    input_2_2: 'Female',
-    input_2_3: 'Other',
-    input_3: 'Email',
-    input_3_1: 'Your Email Address',
-    input_4: 'Birthday',
-    input_4_1: 'Select Date',
-    input_5: 'Select Photo',
-    input_5_error: 'Invalid photo',
-    nextStep: 'Continue',
-    profilePhoto: 'Profile Photo',
+    title: 'פרטים אישיים',
+    input_1: 'שם',
+    input_1_1: 'שם מלא',
+    input_2: 'מגדר',
+    input_2_1: 'גבר',
+    input_2_2: 'אישה',
+    input_2_3: 'אחר',
+    input_3: 'אימייל',
+    input_3_1: 'כתובת האימייל שלך',
+    input_4: 'תאריך לידה',
+    input_4_1: 'בחר/י תאריך',
+    input_5: 'בחר/י תמונה',
+    input_5_error: 'תמונה לא חוקית',
+    nextStep: 'המשך/י',
+    profilePhoto: 'תמונה פרופיל',
   },
   welcomeService: {
-    defProfilePhotoText: 'Profile Photo (optional)',
-    nameError_1: 'Please enter your name',
-    nameError_2: 'Only Letters please',
-    nameError_3: 'Sorry, name is too short',
-    genderError: 'Please select a gender',
-    emailError_1: 'Please enter your email',
-    emailError_2: 'Invalid email address',
-    birthdayError: 'Please enter your birthday'
+    defProfilePhotoText: 'תמונת פרופיל (לא חובה)',
+    nameError_1: 'יש למלא שם',
+    nameError_2: 'רק אותיות בבקשה',
+    nameError_3: 'שם קצר מדי',
+    genderError: 'יש לבחור מגדר',
+    emailError_1: 'חסר אימייל',
+    emailError_2: 'כתובת לא חוקית',
+    birthdayError: 'יש למלא תאריך לידה'
   },
   alertsService: {
     addAsAppAlert: {
-      title: 'Oozdr - Get the app',
-      content: 'Install instantly, find what you need faster, anytime.',
-      dismissText: 'Not now',
-      okText: 'Install'
+      title: 'Oozdr - התקו את האפליקציה',
+      content: 'התקנה מהירה וקלה.',
+      dismissText: 'לא כרגע',
+      okText: 'התקן'
     },
     newVersionAlert: {
-      title: 'New version available',
-      content: 'Please refresh to reload the new version.',
-      dismissText: 'Dismiss',
-      okText: 'Refresh'
+      title: 'קיימת גרסה חדשה',
+      content: 'האם לטעון גרסה חדשה?',
+      dismissText: 'לא כרגע',
+      okText: 'טען'
     },
     FcmMessage: {
       title: '',
@@ -124,16 +124,16 @@ export const DictionaryHebrew = {
       dismissText: '',
       okText: 'Dismiss'
     },
-    reloadingVersionMessage: 'Reloading new version...',
-    pwaAppInstalled: 'Oozdr App installed successfully'
+    reloadingVersionMessage: 'טוען גרסה חדשה...',
+    pwaAppInstalled: 'OOZDR - גרסה חדשה נטענה'
   },
   authService: {
-    logoutConfirmMessage: 'Are you sure you want to log out?',
-    logoutConfirmButton: 'Log out',
-    deleteAcountConfirmMessage: 'Are you sure you want to delete your account and all its data?',
-    deleteAcountConfirmButton: 'Delete',
-    cancelBtn: 'Cancel',
-    deleteActionMessage: 'Deleting your account...'
+    logoutConfirmMessage: 'לצאת מהחשבון?',
+    logoutConfirmButton: 'לצאת',
+    deleteAcountConfirmMessage: 'פעולה זאת תמחוק את חשבונך וכל המידע שבו. האם למחוק?',
+    deleteAcountConfirmButton: 'מחיקה',
+    cancelBtn: 'ביטול',
+    deleteActionMessage: 'מוחק חשבון...'
   },
   connectionsComponent: {
     enableNotifications_1: 'Enable push notifications',
@@ -150,12 +150,12 @@ export const DictionaryHebrew = {
     foundMessage_2: 'You can always make more beats.',
   },
   manageConnectionComponent: {
-    input_1: 'Name',
-    input_1_1: 'Connection Name',
-    input_2: 'Mobile Number',
-    input_2_2: 'Connection Email',
-    input_3: 'Email (Optional)',
-    input_3_1: 'We will send an invitation email stating that someone is looking for them(without revealing who it is from)',
+    input_1: 'שם',
+    input_1_1: 'שם מלא',
+    input_2: 'מספר נייד',
+    input_3: 'אימייל (לא חובה)',
+    input_3_1: 'כתובת אימייל',
+    input_3_2: 'We will send an invitation email stating that someone is looking for them(without revealing who it is from)',
     input_4: 'Welcome Message',
     input_4_1: 'Show them you care. write them something inviting.',
     input_4_2: '(You can change it later)',
@@ -164,8 +164,8 @@ export const DictionaryHebrew = {
     input_5: 'Closure Message',
     input_5_1: 'Write down the things you wish you could say to your ex...',
     input_6: 'Don\'t worry, we are not doing anything yet. They will be notified ONLY if they are looking for you too.',
-    cancelBtn: 'Cancel',
-    okText: 'Ok',
+    cancelBtn: 'ביטול',
+    okText: 'אוקי',
     suffixName: '\'s',
     text_1: 'Closure Message',
     text_2: 'Welcome Message',
@@ -173,10 +173,10 @@ export const DictionaryHebrew = {
     text_4: '\'s Closure Message',
     text_5: 'Your Welcome Message',
     closeBtn: '',
-    saveConnectionButtonText_1: 'Add Beat',
-    saveConnectionButtonText_2: 'Add Closure',
-    saveConnectionButtonText_3: 'Save Closure',
-    saveConnectionButtonText_4: 'Save Beat',
+    saveConnectionButtonText_1: 'הוספה',
+    saveConnectionButtonText_2: 'הוספה',
+    saveConnectionButtonText_3: 'שמירה',
+    saveConnectionButtonText_4: 'שמירה',
     customSelectText: [
       'Hey... Just wondering :)',
       'Hey... After all.. Still thinking about you!',
@@ -185,30 +185,30 @@ export const DictionaryHebrew = {
       'Isn\'t it a pity? to throw it all away?',
       'Passion is momentary, love is enduring'
     ],
-    nameError_1: 'Please Enter a Valid Name',
-    nameError_2: 'Only Letters please',
-    nameError_3: 'Sorry, name is too short',
-    emailError: 'Invalid email address',
+    nameError_1: 'שם לא חוקי',
+    nameError_2: 'רק אותיות בבקשה',
+    nameError_3: 'שם קצר מדי',
+    emailError: 'כתובת לא חוקית',
     closureError: 'Write It Down',
     phoneError_1: 'Enter Connection\'s Mobile Number',
     phoneError_2: 'Can\'t use your own mumber',
-    phoneError_3: 'Invalid Number',
-    phoneError_4: 'Number belongs to',
+    phoneError_3: 'מספר שגוי',
+    phoneError_4: 'מספר שייך ל',
     toastHeader_1: 'is a new beat. Sounds great!',
     toastContent_1_1: 'If',
     toastContent_1_2: 'adds you to their beats, it\'s a groove! and we\'ll update both of you immediately. Let\'s make some music together!',
     toastHeader_2: 'is a new closure for you.',
     toastContent_2: 'is looking for you, we\'ll send them your closure message and update you immediately.',
-    toastDismissButtonText_1: 'got it',
-    toastDismissButtonText_2: 'Ok'
+    toastDismissButtonText_1: 'הבנתי',
+    toastDismissButtonText_2: 'אוקי'
   },
   manageConnectionModalComponent: {
-    title_1: 'Make Beat',
-    title_2: 'Edit Beat',
-    title_3: 'Add Closure',
-    title_4: 'Edit Closure',
-    title_5: 'Closure',
-    helpHeader: 'Adding Closure',
+    title_1: 'יצירת ביט',
+    title_2: 'עריכת ביט',
+    title_3: 'סגירת מעגל - הוספה',
+    title_4: 'סגירת מעגל - עריכה',
+    title_5: 'סגירת מעגל',
+    helpHeader: 'סגירת מעגל',
     helpMessage:
 `
 Writing a closure message can benefit you as well as your EX...
@@ -217,16 +217,16 @@ It's meant to allow you to accept what has happened, say goodbye, and move on.
 <span>Just like Beats, your closure will be matched and shown only if the other person is looking for you too (whether it is to reconnect or to give you their own closure message).</span>
 
 `,
-    textBtn: 'Got It'
+    textBtn: 'הבנתי'
   },
   matchePage: {
     blockHeader: 'You will no longer be able to send or recieve messages from',
     blockMessage: 'Blocked matches can be unblocked at any time (under settings)',
-    blockButtonText: 'Block',
-    confirmCancel: 'Cancel',
+    blockButtonText: 'חסימה',
+    confirmCancel: 'ביטול',
   },
   matchOptionsComponent: {
-    option_1: 'Block'
+    option_1: 'חסימה'
   },
   matchesComponent: {
     noMatches_1: 'No Matches Yet.',
@@ -234,38 +234,38 @@ It's meant to allow you to accept what has happened, say goodbye, and move on.
   },
   chatInputViewComponent: {
     placeholder: 'Say something...',
-    message: 'Ctrl + Enter for next line.'
+    message: 'Ctrl + Enter שורה חדשה'
   },
   profilePage: {
-    saveBtn: 'Save',
-    cancelBtn: 'Cancel'
+    saveBtn: 'שמירה',
+    cancelBtn: 'ביטול'
   },
   settingsPage: {
-    saveBtn: 'Save',
-    cancelBtn: 'Cancel',
-    item_1: 'Notifications',
+    saveBtn: 'שמירה',
+    cancelBtn: 'ביטול',
+    item_1: 'התראות',
     item_1_1: 'Matches Notifications',
     item_1_2: 'So you\'ll know when you have new matches',
     item_2: 'Blocked Matches',
-    item_2_1: 'None',
+    item_2_1: 'אין',
     item_3: 'Invite Your Friends',
-    item_4: 'About',
-    item_4_1: 'Version:',
+    item_4: 'אודות',
+    item_4_1: 'גרסה:',
     item_5: 'Terms of Service',
-    item_5_1: 'Good to know.',
+    item_5_1: 'טוב לדעת',
     item_6: 'Privacy Policy',
-    item_6_1: 'Please read.',
-    item_7: 'Support',
-    item_7_1: 'Get help, contact us.',
-    item_8: 'Log Out',
-    item_9: 'Delete Account',
-    unBlockHeader: 'Unblock',
-    unBlockButtonText: 'Unblock',
-    unblockButtonCancel: 'cancel'
+    item_6_1: 'בבקשה לקרוא',
+    item_7: 'תמיכה',
+    item_7_1: 'קבלת עזרה ויצירת קשר',
+    item_8: 'יציאה',
+    item_9: 'מחיקת חשבון',
+    unBlockHeader: 'לבטל חסימה',
+    unBlockButtonText: 'לבטל חסימה',
+    unblockButtonCancel: 'ביטול'
   },
   socialShareComponent: {
-    copied: 'Copied',
-    invite: 'Invite',
+    copied: 'הועתק',
+    invite: 'הזמן/י',
     share: {
       title: 'App recommendation for you',
       text: 'This app has helped me to reconnect with someone dear and I think it can help you as well.'
@@ -277,23 +277,23 @@ It's meant to allow you to accept what has happened, say goodbye, and move on.
     text_3: 'Who will you invite?'
   },
   supportPage: {
-    title: 'Support'
+    title: 'תמיכה'
   },
   supportComponent: {
-    how_title: 'How Oozdr?'
+    how_title: 'איך OOZDR?'
   },
   contactPage: {
-    title: 'Contact'
+    title: 'יצירת קשר'
   },
   contactComponent: {
     title: 'Have Some Questions?',
     subTitle_1: 'We’re here to help answer any question you might have.',
     subTitle_2: 'You can reach us by social media, email or directly using this form.',
-    placeholder_1: 'Your name',
-    placeholder_2: 'What\'s your email?',
-    placeholder_3: 'How can we help?',
-    sendBtn: 'SEND',
-    sendMessageHeader: 'THANK YOU!',
-    sendMessage: 'We have received your question/feedback'
+    placeholder_1: 'שם מלא',
+    placeholder_2: 'כתובת אימייל',
+    placeholder_3: 'איך אפשר לעזור?',
+    sendBtn: 'שליחה',
+    sendMessageHeader: 'תודה רבה!',
+    sendMessage: 'קיבלנו את הפנייה'
   }
 }
