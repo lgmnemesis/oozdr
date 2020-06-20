20fbd7ee3133e5c052a9b4ad6d44c40057e54df6
lgm@nemesis.co.il <lgm@nemesis.co.il>
2020-06-20 13:59:35 +0300

    deploy

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -14,7 +14,7 @@ export const environment = {
     appId: "1:15338544369:web:870238a038bbc520561d50",
     measurementId: "G-XPLGQ5LX96"
   },
-  clientVersion: '2.0.96'
+  clientVersion: '2.0.98'
 };
 
 /*
