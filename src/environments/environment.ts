6ac54b1181aa52c7436ac05babb21e78642ca482
lgm@nemesis.co.il <lgm@nemesis.co.il>
2020-07-22 11:48:17 +0300

    npm update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -33,7 +33,7 @@ export const environment = {
   get firebaseConfig() {
     return getConfig();
   },
-  clientVersion: '2.0.118'
+  clientVersion: '2.0.119'
 };
 
 function isEmulator(): boolean {
